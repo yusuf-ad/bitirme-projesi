3ed1380a7c2aa0888effdd9d487115702ee9b529
Osman İleri <ileriiosman@gmail.com>
2025-10-09 18:00:17 +0300

    Fix typo in onboarding page title from "Definee Your goals" to "Define Your goals" for consistency in messaging.

--- app/(onboarding)/profile/index.tsx
+++ app/(onboarding)/profile/index.tsx
@@ -6,7 +6,7 @@ import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 const onboardingPages = [
   {
-    title: "Define Your goals",
+    title: "Definee Your goals",
     description:
       "Let's get started! Choose your main goal to reach results faster and stay motivated",
   },
