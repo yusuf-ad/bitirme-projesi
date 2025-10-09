3e43936c1bf29a52dd856da33d2b862d04f0482e
yusuf-ad <yusufgad24@gmail.com>
2025-10-09 18:18:44 +0300

    test

--- app/(onboarding)/profile/index.tsx
+++ app/(onboarding)/profile/index.tsx
@@ -6,7 +6,7 @@ import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 const onboardingPages = [
   {
-    title: "Define Your goals",
+    title: "Define Your goalsss",
     description:
       "Let's get started! Choose your main goal to reach results faster and stay motivated",
   },
