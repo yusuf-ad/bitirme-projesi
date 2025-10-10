82a6c4efe440fef9f0bbfaedb9f5842c15c6a435
yusuf-ad <yusufgad24@gmail.com>
2025-10-10 08:52:29 +0300

    Remove obsolete profile screen from onboarding flow and update navigation structure to streamline user experience.

--- app/(onboarding)/_layout.tsx
+++ app/(onboarding)/_layout.tsx
@@ -4,7 +4,6 @@ export default function OnboardingLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="profile/index" options={{ headerShown: false }} />
       <Stack.Screen name="flow" options={{ headerShown: false }} />
     </Stack>
   );
