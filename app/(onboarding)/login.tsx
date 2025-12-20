5ee3ee73bcafdbe795296e6c57c71551673613ab
YunusKok <ymert.kok@std.hku.edu.tr>
2025-12-21 01:12:38 +0300

    feat: add initial login and signup screens with authentication forms and UI components.

--- app/(onboarding)/login.tsx
+++ app/(onboarding)/login.tsx
@@ -1,25 +1,16 @@
 import { LoginTab } from "@/features/auth/components/login-tab";
 import { StyleSheet, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function LoginScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <LoginTab />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <LoginTab />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
-  },
-  content: {
-    flex: 1,
-    paddingHorizontal: 24,
-    paddingTop: 16,
   },
 });
